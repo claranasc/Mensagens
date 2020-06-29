57539e4adbf1a192a604a9264e90cb2025437a1c
Clara Nascimento <clara.nascimento@outlook.com>
2020-06-29 09:29:30 -0300

    Add BaseViewController

---
+++ Mensagens/BaseViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BaseViewController.swift
+//  Mensagens
+//
+//  Created by Clara Nascimento on 29/06/20.
+//  Copyright © 2020 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    @IBOutlet weak var lbMessage: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func changeColor (_sender: UIButton) {
+        
+    }
+
+}
